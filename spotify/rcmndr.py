04bedefb368f126e326794fb5731cc59f105db8d
David Poole <14233215+davidlpoole@users.noreply.github.com>
2024-03-06 11:03:46 +1300

    Extract artist and track details from recommendations

--- spotify/rcmndr.py
+++ spotify/rcmndr.py
@@ -12,13 +12,22 @@ def main():
     artist_list = ["Geju", "Merkaba"]
     artist_id_list = get_artist_ids(artist_list)
     seeds_object = create_seeds_object(artist_id_list)
-    recommendations = get_recommendations(seeds_object)
-    recommendations_df = pd.DataFrame(recommendations["tracks"])
+    recs = get_recommendations(seeds_object)
+    recs_df = pd.DataFrame(recs["tracks"])
 
-    # print each row of recommendations_df
-    for index, row in recommendations_df.iterrows():
-        print(row)
-        print("\n")
+    result_df = pd.DataFrame(
+        {
+            "album_type": recs_df.album.apply(lambda x: x["album_type"]),
+            "artists": recs_df.album.apply(
+                lambda x: ", ".join([artist["name"] for artist in x["artists"]])
+            ),
+            "track_name": recs_df.name,
+            "open_spotify_url": recs_df.external_urls.apply(lambda x: x["spotify"]),
+            # "preview_url": recs_df.preview_url,
+        }
+    )
+
+    print(result_df)
 
 
 if __name__ == "__main__":
